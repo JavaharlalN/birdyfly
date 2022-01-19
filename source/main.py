e494ea1a8a19338fca58ae56ef7ca666150a81c5
Javaharlal Nehru <91618941+JavaharlalN@users.noreply.github.com>
2022-01-19 17:07:44 +0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -2,19 +2,28 @@ import pygame as pg
 from classes import *
 
 pg.init()
-w, h = 300, 300
-scr = pg.display.set_mode((w, h), pg.RESIZABLE)
+info = pg.display.Info()
+w, h = info.current_w, info.current_h
+scr = pg.display.set_mode((w, h), pg.FULLSCREEN)
 cursor = Cursor(0, 0)
-startButton = Button(150 - 88, 150 - 26, 176, 52)
-startButtonImage = pg.image.load("play.png")
+startButton = Button(0, 0, cut_sprite(49, 75, 35, 8))
+startButton.moveTo(w // 2, h // 2)
 scene = 0  # 0 - меню, 1 - настройки, 2 - игра, 3 - экран поражения
+bird = Bird(5, 1)
+tube = Tube(w, h)
 pg.font.init()
+pg.mouse.set_visible(False)
 fps = 64
 clock = pg.time.Clock()
+isMouseDown = False
+scale = 4
+clickTimeout = 200
 run = True
+ms = 0
 while run:
     scr.fill((255, 255, 255))
     for event in pg.event.get():
+        isMouseDown = event.type == pg.MOUSEBUTTONDOWN
         if event.type == pg.QUIT:
             run = False
         elif event.type == pg.MOUSEMOTION:
@@ -23,9 +32,34 @@ while run:
             w, h = event.size
             if scene == 0:
                 startButton.moveTo(w // 2, h // 2)
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                if scene == 0:
+                    run = False
+                else:
+                    scene = 0
+            elif event.key == pg.K_SPACE and scene == 2:
+                tube.jump()
     if scene == 0:
-        scr.blit(startButtonImage, (startButton.x, startButton.y))
-        pg.draw.rect(scr, (0, 153, 255), (startButton.x - 10, startButton.y - 10, startButton.w, startButton.h), 2)
+        cursor.calc(ms)
+        cursor.draw(scr)
+        if cursor.inBtn(startButton):
+            if not cursor.rect:
+                # cursor.setPos(startButton.x + startButton.w // 2, startButton.y + startButton.h // 2)
+                cursor.setPos(startButton.x + startButton.w * scale / 2 + 10, startButton.y + startButton.h * scale / 2 + 10)
+                cursor.rect = True
+                cursor.setSize(startButton.w * scale + 24, startButton.h * scale + 24)
+            if isMouseDown:
+                scene = 2
+                cursor.reset()
+        else:
+            cursor.reset()
+        startButton.draw(scr, 4)
+    elif scene == 2:
+        tube.tick(ms, scale)
+        tube.draw(scr, scale)
+        bird.tick(ms)
+        bird.draw(scr, scale)
     pg.display.flip()
-    clock.tick(fps)
+    ms = clock.tick(fps)
 pg.quit()
